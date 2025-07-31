ad3bf8c02ed0c6a08bd33d546dfc68423c29f006
Mateus Pim <mateuspimsantos@gmail.com>
2025-07-31 20:37:32 -0300

    Add story router to fastapi main

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
-from routers import job
+from routers import job, story
 from db.database import create_tables
 
 create_tables()
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+app.include_router(story.router, prefix=settings.API_PREFIX)
 app.include_router(job.router, prefix=settings.API_PREFIX)
 
 if __name__ == "__main__":

48e2becc70944ee7bb8c0e5aabd4817989c9ac8c
Mateus Pim <mateuspimsantos@gmail.com>
2025-07-31 20:34:25 -0300

    Add create tables method when starting server

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
+from db.database import create_tables
+
+create_tables()
 
 app = FastAPI(
     title="Choose Your Own Adventure Game API",
@@ -16,9 +19,10 @@ app.add_middleware(
     allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", host="0.0.0.0", port=8008, reload=True)

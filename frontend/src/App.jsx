a404d0f726e392823b089de9ade21adb54f091ff
Mateus Pim <mateuspimsantos@gmail.com>
2025-08-01 02:44:03 -0300

    Add router to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
 import "./App.css";
+import  {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import StoryLoader from "./components/StoryLoader";
+import StoryGenerator from "./components/StoryGenerator";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <div className="app-container">
+        <header>
+          <h1>
+            Interactive Story Generator
+          </h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<StoryGenerator />} />
+            <Route path="/story/:id" element={<StoryLoader />} />
+           </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
